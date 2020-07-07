af493186d0cb73c1b769ca6a904c3a04fb5b971a
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-06 21:27:56 -0300

    Initial commit

---
+++ Day 1/hello.go
@@ -0,0 +1,11 @@
+// Every program should have a main package
+package main
+
+//fmt stands for fumpt. It implements formatted I/O functions similar to C
+import "fmt"
+
+// Every program should have a main function. Its the first executed function.
+func main() {
+	// Println prints the phrase on the standard output and also break a line.
+	fmt.Println("Hello Gophers!!")
+}
