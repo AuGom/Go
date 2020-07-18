5b8aaf3321800bac856eb8ab860fd8263c52e701
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-18 11:50:07 -0300

    Day 17 - Recursion

---
+++ Day 17/recursion.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// function to calculate factorial recursively
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(5))
+}
